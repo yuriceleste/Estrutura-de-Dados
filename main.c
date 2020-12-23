795b453ddd51f2cfe590bb4596cee397a3c10a70
yuripulier <50811727+yuripulier@users.noreply.github.com>
2020-12-23 11:24:42 -0300

    Update main.c

--- main.c
+++ main.c
@@ -48,7 +48,7 @@ int comparafloat(void *s1, void *s2) {
         return 0;
 }
 */
-// funcao que imprime a matricula do aluno
+// funcao que imprime a matricula do cliente
 void imprimeCliente(void* c){
     Cliente* temp = (Cliente*)c;
     printf("%d", temp->identidade);
